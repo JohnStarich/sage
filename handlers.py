3b7bd2bf9f672887c50375f9136b20aa9f0cd549
John Starich <johnstarich@johnstarich.com>
2019-04-16 02:01:07 -0500

    Ensure consistent account lookup from OFX file

--- handlers.py
+++ handlers.py
@@ -3,20 +3,16 @@ from functools import lru_cache
 from funcs import func_chain, map, split
 from ledger import AccountStatement, LedgerTransaction
 from ofxclient import Account as ClientAccount, BankAccount, \
-        CreditCardAccount, BrokerageAccount, Institution as ClientInstitution
+        CreditCardAccount, BrokerageAccount
 from ofxclient.config import OfxConfig
 from ofxparse import Account, OfxParser, Statement
-from typing import Iterable, Union
+from typing import Iterable
 
 import sys
 
 
-@lru_cache(maxsize=20)
-def _ofx_account_name(account: Union[Account, ClientAccount]) -> str:
-    if isinstance(account, Account):
-        institution = ClientInstitution.deserialize(
-                account.institution.__dict__)
-        account = ClientAccount.from_ofxparse(account, institution)
+@lru_cache()
+def _ofx_account_name(account: ClientAccount) -> str:
     institution_str = account.institution.description
     account_str = account.description
     if isinstance(account, BankAccount):
@@ -68,18 +64,27 @@ class OfxFiles(OfxHandler):
         self._file_names = file_names
         self._account_map = {}
         for account in config.accounts():
-            self._account_map[account.number] = account
+            institution = account.institution.id
+            if institution not in self._account_map:
+                self._account_map[institution] = {}
+            self._account_map[institution][account.number] = account
 
-    def account_name(self, account: Account) -> str:
+    def _lookup_account(self, account: Account) -> ClientAccount:
+        institution = account.institution.fid
         account_id = account.account_id
-        if account_id in self._account_map:
-            account = self._account_map[account_id]
-            return _ofx_account_name(account)
-        else:
+        if institution in self._account_map and \
+                account_id in self._account_map[institution]:
+            return self._account_map[institution][account_id]
+        return None
+
+    def account_name(self, raw: Account) -> str:
+        account = self._lookup_account(raw)
+        if account is None:
             return '%s %s' % (
-                account.institution.organization,
-                account_id,
+                raw.institution.organization,
+                raw.account_id,
             )
+        return _ofx_account_name(account)
 
     def statement(self, account: Account) -> Statement:
         return account.statement
