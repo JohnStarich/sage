50fa3a032cb5775b7bf4329c70cdad184af3ee84
John Starich <johnstarich@johnstarich.com>
2019-06-09 23:07:58 -0500

    Add coverage tracking

--- coverage.sh
+++ coverage.sh
@@ -9,9 +9,16 @@ function do_exit() {
 
 trap do_exit EXIT
 
+set -e
+
 go test ./... -race -cover -coverprofile "$tmpfile" >&2
 coverage=$(go tool cover -func "$tmpfile" | tail -1 | awk '{print $3}')
 echo '##########################' >&2
 printf '### Coverage is %6s ###\n' "$coverage" >&2
 echo '##########################' >&2
 echo "$coverage"
+
+if [[ -n "$COVERALLS_TOKEN" ]]; then
+    go get github.com/mattn/goveralls
+    goveralls -coverprofile="$tmpfile" -service=travis-ci -repotoken "$COVERALLS_TOKEN"
+fi
