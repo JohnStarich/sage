25141e80413c2da90e89a0a3d0357099307bbe01
John Starich <johnstarich@johnstarich.com>
2019-06-11 00:23:02 -0500

    Add client cache tests

---
+++ client/cache_test.go
@@ -0,0 +1,21 @@
+package client
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestClientCache(t *testing.T) {
+	inst := institution{url: "some URL"}
+	client, err := clientForInstitution(inst)
+	require.NoError(t, err)
+
+	client2, err := clientForInstitution(inst)
+	require.NoError(t, err)
+	assert.True(t, client == client2, "The client must be the same pointer")
+
+	_, err = clientForInstitution(institution{config: Config{OFXVersion: "not a real version"}})
+	assert.Error(t, err)
+}
