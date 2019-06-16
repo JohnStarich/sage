c8c0a6ede1fe222599eeb407ea4544ee2d552dd2
John Starich <johnstarich@johnstarich.com>
2019-06-16 16:19:39 -0500

    Pretty up balance table, only show assets and liabilities

---
+++ web/src/Amount.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './Amount.css';
+
+export default function(props) {
+  let amount = props.amount
+  if (typeof amount !== 'number') {
+    return "NaN"
+  }
+  let className = "amount"
+  let sign = ""
+  let [integer, fractional] = amount.toFixed(2).split(".")
+  if (amount < 0) {
+    sign = "-"
+    integer = integer.slice(1)
+    className += " amount-negative"
+  }
+  let newAmount = Array.from(integer)
+    .reverse()
+    .map((ch, i) => {
+      if (i !== 0 && i !== integer.length && i % 3 === 0) {
+        return ch + "," 
+      }
+      return ch
+    })
+    .reverse()
+    .join("")
+  let commaBlocks = newAmount.split(',')
+  return (
+    <span className={className}>
+      <span className="amount-prefix">{props.prefix}</span>
+      <span className="amount-sign">{sign}</span>
+      {commaBlocks[0]}
+      {commaBlocks.slice(1).map((group, i) =>
+        <span key={i}>
+          <span className="amount-thousands">,</span>
+          {group}
+        </span>
+      )}
+      {"."+fractional}
+    </span>
+  )
+}
--- web/src/Balances.js
+++ web/src/Balances.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import './Balances.css';
+import Amount from './Amount';
 
 export default class Balances extends React.Component {
   state = {
@@ -21,15 +23,38 @@ export default class Balances extends React.Component {
   render() {
     return (
       <div className="balances">
-        <ul>
-          {Object.entries(this.state.balances)
-              .map(bal => {
-                let [account, balanceItems] = bal
-                let balance = balanceItems[balanceItems.length-1]
-                return <li key={account}>{account} -- $ {balance}</li>
-              })
-          }
-        </ul>
+        <table>
+          <tbody>
+            <tr><th>Account</th><th>Balance</th></tr>
+            {Object.entries(this.state.balances)
+                .filter(bal => {
+                  let fullAccount = bal[0]
+                  return fullAccount.startsWith("assets:") || fullAccount.startsWith("liabilities:")
+                })
+                .map(bal => {
+                  let [fullAccount, balanceItems] = bal
+
+                  let className = ""
+                  let balance = Number(balanceItems[balanceItems.length-1])
+                  let accountType = ""
+                  let account = fullAccount
+                  let firstColon = account.indexOf(':')
+                  if (firstColon > 0) {
+                    accountType = account.slice(0, firstColon)
+                    account = account.slice(firstColon + 1)
+                    className += " account-" + accountType
+                  }
+                  account = account.replace(":", " - ")
+                  return (
+                    <tr key={fullAccount}>
+                      <td className={className}>{account}</td>
+                      <td><Amount amount={balance} prefix='$' /></td>
+                    </tr>
+                  )
+                })
+            }
+          </tbody>
+        </table>
       </div>
     )
   }
