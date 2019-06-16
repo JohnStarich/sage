d189d8fc390e10484588684101971ccb24c5b7b5
John Starich <johnstarich@johnstarich.com>
2019-06-15 23:50:25 -0500

    Initial commit for React app

---
+++ web/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello, World!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
