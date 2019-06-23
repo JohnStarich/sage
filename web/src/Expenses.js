05cf7a83930d1a6637ceada5464e16fc92526c31
John Starich <johnstarich@johnstarich.com>
2019-06-23 01:29:58 -0500

    Improve exp/rev graph colors, reduce number of categories, improve tooltips

--- web/src/Expenses.js
+++ web/src/Expenses.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import axios from 'axios';
-import { BarChart, Bar, ResponsiveContainer, Tooltip, XAxis, YAxis, Legend } from 'recharts';
+import {
+  Bar,
+  BarChart,
+  Legend,
+  ReferenceLine,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
 import Amount from './Amount';
-
-const colors = [
-  'green',
-  'lime',
-  'orange',
-  'purple',
-  'cyan',
-  'blue',
-  'magenta',
-  'violet',
-  'teal',
-  'brown',
-  'coral',
-  'darkgoldenrod',
-]
+import Colors from './Colors';
+import './Expenses.css';
 
 export default class Expenses extends React.Component {
   state = {}
@@ -31,58 +27,184 @@ export default class Expenses extends React.Component {
         if (res.status !== 200 ) {
           throw new Error("Error fetching balances")
         }
+        res.data.Accounts = res.data.Accounts.map(account => {
+          account.Balances = account.Balances.map(Number)
+          return account
+        })
         this.setState(res.data)
       })
   }
 
   render() {
-    let start = new Date(this.state.Start).getTime()
-    let end = new Date(this.state.End).getTime()
-    if (! this.state.Accounts || end < start) {
-      return <div>No expense data to display</div>
+    const noData = <div>No expense data to display</div>
+    if (! this.state.Accounts) {
+      return noData
     }
-    let interval = (end - start) / this.state.Accounts[0].Balances.length
-    let times = []
-    for (let i = start; i < end; i += interval) {
-      times.push(new Date(i));
+    let payload = Object.assign({}, this.state)
+    payload.Accounts = reduceCategories(payload.Accounts)
+    payload.Accounts = payload.Accounts
+        .map(removeCumulative)
+        .map(negateBalances)
+    payload.Accounts = sortAccountsByActivity(payload.Accounts)
+    let data = convertAccountsToChartData(payload)
+    if (data === null) {
+      return noData
     }
-
-    let data = times.map((time, i) =>
-      this.state.Accounts.reduce((accumulator, account) => {
-        let previous = i === 0 ? 0 : account.Balances[i - 1]
-        // negate the balance since expense and revenue accounts are reversed
-        accumulator[account.Account] = - (account.Balances[i] - previous)
-        return accumulator
-      }, { Date: new Date(time).toDateString() })
-    )
     return (
-      <div style={{ margin: "1em" }}>
-        <ResponsiveContainer height={700} width="100%">
-          <BarChart title="hi" data={data} stackOffset="sign" margin={{ left: 50, top: 5, right: 5, bottom: 5 }}>
-            {this.state.Accounts.map((a, i) =>
-              <Bar key={a.ID} dataKey={a.Account} stackId="1" fill={colors[i % colors.length]} />
+      <div className="expenses">
+        <ResponsiveContainer width="100%">
+          <BarChart data={data} stackOffset="sign" margin={{ left: 50 }}>
+            {payload.Accounts.map((a, i) =>
+              <Bar key={a.ID} dataKey={a.Account} stackId="1" fill={Colors[i % Colors.length]} />
             )}
             <XAxis dataKey="Date" />
-            <YAxis tick={tick => {
-              let payload = tick.payload;
-              // copy nearly all of original tick attributes
-              // filter out any invalid attributes
-              [
-                'verticalAnchor',
-                'visibleTicksCount',
-                'payload',
-              ].forEach(k => delete tick[k])
-              return (
-                <text {...tick}>
-                  <Amount prefix='$' amount={Number(payload.value)} tagName='tspan' />
-                </text>
-              )
-            }} />
-            <Tooltip />
+            <YAxis tick={<AmountTick />} />
+            <ReferenceLine y={0} />
+            <Tooltip content={<AmountTooltip />} />
             <Legend />
           </BarChart>
         </ResponsiveContainer>
       </div>
     );
   }
+}
+
+const AmountTooltip = ({ active, payload, label }) => {
+  if (! active) {
+    return null
+  }
+  return (
+    <div className="amount-tooltip">
+      <p className="label">{label}</p>
+      <ul>
+        {
+          payload
+            .filter(account => account.value !== 0)
+            .map((account, i) =>
+              <li key={i} className="entry" style={{ color: account.fill }}>
+                <span className="account-name">{account.name} : </span>
+                <Amount amount={account.value} prefix='$' />
+              </li>
+            )
+        }
+      </ul>
+    </div>
+  )
+}
+
+const AmountTick = tick => {
+  let payload = tick.payload;
+  // copy nearly all of original tick attributes
+  // filter out any invalid attributes
+  tick = Object.assign({}, tick);
+  [
+    'verticalAnchor',
+    'visibleTicksCount',
+    'payload',
+  ].forEach(k => delete tick[k])
+  return (
+    <text {...tick}>
+      <Amount prefix='$' amount={payload.value} tagName='tspan' />
+    </text>
+  )
+}
+
+function convertAccountsToChartData(payload) {
+  let start = new Date(payload.Start).getTime()
+  let end = new Date(payload.End).getTime()
+  if (! payload.Accounts || end < start) {
+    return null
+  }
+  let interval = (end - start) / payload.Accounts[0].Balances.length
+  let times = []
+  for (let i = start; i < end; i += interval) {
+    times.push(new Date(i));
+  }
+
+  // convert from series of balances and times into large data point objects
+  return times.map((time, i) =>
+    payload.Accounts.reduce((accumulator, account) => {
+      accumulator[account.Account] = account.Balances[i]
+      return accumulator
+    }, { Date: new Date(time).toDateString() })
+  )
+}
+
+function removeCumulative(account) {
+  return Object.assign({}, account, {
+    Balances: account.Balances.map((balance, index) => {
+      let previousBalance = index === 0 ? 0 : account.Balances[index - 1]
+      return balance - previousBalance
+    })
+  })
+}
+
+// negate the balance since expense and revenue accounts are reversed
+function negateBalances(account) {
+  return Object.assign({}, account, {
+    Balances: account.Balances.map(balance => - balance)
+  })
+}
+
+function reduceCategories(accounts) {
+  let accountNames = accounts.map(a => a.Account)
+  accountNames = reduceCategoryNames(accountNames)
+  let newAccounts = {}
+  for (let account of accounts) {
+    for (let name of accountNames) {
+      if (account.Account === name || account.Account.startsWith(name+':')) {
+        if (newAccounts[name] === undefined) {
+          account.Account = name
+          newAccounts[name] = account
+        } else {
+          newAccounts[name].Balances =
+            newAccounts[name].Balances
+              .map((balance, i) => balance + account.Balances[i])
+        }
+      }
+    }
+  }
+  return Object.values(newAccounts)
+}
+
+// try to reduce graph complexity by combining accounts with identical prefixes
+function reduceCategoryNames(names) {
+  const targetCount = 10
+
+  let allNames = new Set()
+  for (let name of names) {
+    if (name.includes(':')) {
+      allNames.add(name.slice(0, name.indexOf(':')))
+    } else {
+      allNames.add(name)
+    }
+  }
+
+  let previousSize = 0
+  while (allNames.size < targetCount && previousSize !== allNames.size) {
+    previousSize = allNames.size
+    for (let name of allNames) {
+      let prefix = name+':'
+      let foundPrefixMatch = false
+      for (let i = 0; i < names.length && allNames.size < targetCount; i++) {
+        if (names[i].startsWith(prefix)) {
+          allNames.add(names[i])
+          foundPrefixMatch = true
+        }
+      }
+      if (foundPrefixMatch) {
+        allNames.delete(name)
+        break
+      }
+    }
+  }
+  return allNames
+}
+
+// assumes current balances are not cumulative
+function sortAccountsByActivity(accounts) {
+  const getBalance = a =>
+    a.Balances.map(Math.abs)
+      .reduce((acc, elem) => acc + elem)
+  return accounts.sort((a, b) => getBalance(b) - getBalance(a))
 }
