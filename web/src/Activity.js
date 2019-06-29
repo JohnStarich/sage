f28bba4f03043d1cf85b8a409351229bc10b61d6
John Starich <johnstarich@johnstarich.com>
2019-06-29 17:37:22 -0500

    Add initial react router

---
+++ web/src/Activity.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+import Balances from './Balances';
+import Expenses from './Expenses';
+import Transactions from './Transactions';
+
+export default function Activity(props) {
+  const { syncTime } = props;
+  return (
+      <Container className="content">
+        <Row>
+          <Col lg xl={5}><Balances syncTime={syncTime} /></Col>
+          <Col xl={7}><Expenses syncTime={syncTime} /></Col>
+        </Row>
+        <Row>
+          <Col><Transactions syncTime={syncTime} /></Col>
+        </Row>
+      </Container>
+  )
+}
--- web/src/App.js
+++ web/src/App.js
@@ -1,51 +1,42 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route, Link, NavLink } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
 import Dropdown from 'react-bootstrap/Dropdown';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Balances from './Balances';
-import Expenses from './Expenses';
-import Transactions from './Transactions';
+import Nav from 'react-bootstrap/Nav';
 import Sync from './Sync';
+import Activity from './Activity';
 
 function App() {
   const [syncTime, setSyncTime] = React.useState(new Date())
   return (
-    <div className="App">
-      <Navbar bg="dark" expand="sm" variant="dark" sticky="top">
-        <Navbar.Brand href="#">Sage</Navbar.Brand>
+    <Router>
+      <Navbar className="main-nav" bg="dark" expand="sm" variant="dark" sticky="top">
+        <NavLink exact to="/"><Navbar.Brand>Sage</Navbar.Brand></NavLink>
         <Navbar.Toggle />
         <Navbar.Collapse>
           <Nav className="mr-auto">
-            <Nav.Link href="#">Activity</Nav.Link>
+            <NavLink exact to="/" className="nav-link">Activity</NavLink>
           </Nav>
           <Sync className="mr-2" onSync={() => setSyncTime(new Date())} />
           <Dropdown bg="dark" alignRight>
             <Dropdown.Toggle>
-              Account
+              Settings
             </Dropdown.Toggle>
             <Dropdown.Menu>
-              <Dropdown.Item href="#action/3.1">Account</Dropdown.Item>
-              <Dropdown.Item href="#action/3.2">Categories</Dropdown.Item>
+              <Link to="/accounts"><Dropdown.Item as="button">Accounts</Dropdown.Item></Link>
+              <Link to="/categories"><Dropdown.Item as="button">Categories</Dropdown.Item></Link>
             </Dropdown.Menu>
           </Dropdown>
         </Navbar.Collapse>
       </Navbar>
-      <Container className="content">
-        <Row>
-          <Col lg xl={5}><Balances syncTime={syncTime} /></Col>
-          <Col xl={7}><Expenses syncTime={syncTime} /></Col>
-        </Row>
-        <Row>
-          <Col><Transactions syncTime={syncTime} /></Col>
-        </Row>
-      </Container>
-    </div>
+      
+      <Route path="/" exact component={() => <Activity syncTime={syncTime} />} />
+      <Route path="/accounts" component={() => null} />
+      <Route path="/categories" component={() => null} />
+    </Router>
   );
 }
 
