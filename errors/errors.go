e18f85573dd4a245d30bfe55c22b63b993adf078
John Starich <johnstarich@johnstarich.com>
2019-08-22 00:09:17 -0500

    Add utility funcs for easy validation error handling

--- errors/errors.go
+++ errors/errors.go
@@ -1,9 +1,36 @@
 package errors
 
-import "strings"
+import (
+	"strings"
+
+	"github.com/pkg/errors"
+)
 
 // Errors makes it easy to combine multiple errors into a single string
 type Errors []error
+
+// ErrIf appends an error with failureMessage if the condition is true
+// Returns the condition to allow for further conditional checks
+func (e *Errors) ErrIf(condition bool, failureMessage string, formatArgs ...interface{}) bool {
+	if condition {
+		*e = append(*e, errors.Errorf(failureMessage, formatArgs...))
+	}
+	return condition
+}
+
+func (e *Errors) AddErr(err error) bool {
+	if err != nil {
+		*e = append(*e, err)
+	}
+	return err == nil
+}
+
+func (e Errors) ErrOrNil() error {
+	if len(e) > 0 {
+		return e
+	}
+	return nil
+}
 
 func (e Errors) Error() string {
 	var buf strings.Builder
