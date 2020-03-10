79cd70f3d836907c40a21d46fe36c1786a9f8220
John Starich <johnstarich@johnstarich.com>
2020-03-10 00:46:29 -0500

    Add sync error button and modal

--- web/src/Sync.js
+++ web/src/Sync.js
@@ -1,48 +1,99 @@
 import API from './API';
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Modal from 'react-bootstrap/Modal';
 import Spinner from 'react-bootstrap/Spinner';
 
 import './Sync.css';
 
-export default function Sync(props) {
+async function getStatus() {
+  try {
+    return (await API.get('/v1/getLedgerSyncStatus')).data
+  } catch (e) {
+    console.error(e)
+    return
+  }
+}
+
+export default function Sync({ className, onSync }) {
   const [isSyncing, setSyncing] = React.useState(false)
   const [error, setError] = React.useState(null)
+  const [showError, setShowError] = React.useState(false)
 
   React.useEffect(() => {
-    setInterval(() => {
-      API.get('/v1/getLedgerSyncStatus')
-        .then(res => {
-          setSyncing(res.data.Syncing)
-          setError(res.data.Error)
-        })
-    }, 10000)
+    const checkSync = async () => {
+      const status = await getStatus()
+      if (onSync && isSyncing && !status.Syncing) {
+        onSync()
+      }
+      setSyncing(status.Syncing)
+      setError(status.Error || null)
+    }
+
+    const interval = setInterval(() => checkSync(), 10000)
+    return () => clearInterval(interval)
+  }, [isSyncing, onSync])
+  React.useEffect(() => {
+    getStatus().then(status => {
+      setSyncing(status.Syncing)
+      setError(status.Error || null)
+    })
   }, [])
 
   const clickSync = () => {
-    API.post('/v1/syncLedger')
     setSyncing(true)
+    setError(null)
+    API.post('/v1/syncLedger')
   }
 
-  let className = "sync"
-  if (props.className) {
-    className += " " + props.className
+  let classNames = ["sync"]
+  if (className) {
+    classNames.push(className)
   }
   if (error) {
-    className += " sync-failed"
+    classNames.push("sync-failed")
+  }
+
+  let buttonVariant = "dark"
+  if (error) {
+    buttonVariant = "danger"
   }
 
   return (
-    <Button
-      variant="dark"
-      className={className}
-      disabled={isSyncing}
-      onClick={!isSyncing ? clickSync : null}
-    >
-      {isSyncing
-        ? <Spinner animation="border" size="sm" />
-        : (error ? 'Sync Failed' : 'Sync')
-      }
-    </Button>
+    <div className={classNames.join(' ')}>
+      <ButtonGroup>
+        <Button
+          variant={buttonVariant}
+          className="sync-title"
+          disabled={isSyncing}
+          onClick={!isSyncing ? clickSync : null}
+        >
+          {isSyncing
+            ? <Spinner animation="border" size="sm" />
+            : 'Sync'
+          }
+        </Button>
+        {error ?
+          <Button
+            variant={buttonVariant}
+            className="sync-info"
+            onClick={() => setShowError(true)}
+          >
+            <span className="sync-info-wrapper">
+              <span className="sync-info-text">ℹ</span>
+            </span>
+          </Button>
+          : null}
+      </ButtonGroup>
+      <Modal show={showError} onHide={() => setShowError(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Sync error</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <pre className="sync-error"><code>{error}</code></pre>
+        </Modal.Body>
+      </Modal>
+    </div>
   )
 }
